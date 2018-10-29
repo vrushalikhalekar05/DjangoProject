d9a85490644ddc02aadea5b045096f0c543ee2a5
vrushalikhalekar05 <vrushalikhalekar05@gmail.com>
2018-10-29 22:08:35 +0530

    done login api

---
+++ unipune/user/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+
+from .managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    date_joined = models.DateTimeField( auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    contact_no = models.CharField(max_length=30, blank=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
---
+++ webproject/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/vrushali/DjangoProject/webproject/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
