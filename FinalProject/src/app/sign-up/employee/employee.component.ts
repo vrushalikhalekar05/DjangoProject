1efdc13f7b7d31b2a54f99c3229065426906f352
vrushalikhalekar05 <vrushalikhalekar05@gmail.com>
2018-11-21 18:24:27 +0530

    localstorage

--- FinalProject/src/app/sign-up/employee/employee.component.ts
+++ FinalProject/src/app/sign-up/employee/employee.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 import { EmployeeService } from '../shared/employee.service';
 import { Http, Headers, Response } from '@angular/http';
-
+import { tokenPara } from '../tokenPara';
 import { NgForm } from '@angular/forms';
 //import { FirebaseObjectObservable,FirebaseListObservable} from 'angularfire2/database-deprecated';
 //import { Employee } from '../shared/employee.model';
@@ -17,17 +17,27 @@ import { Router } from '@angular/router';
  providers:[EmployeeService]
 })
 export class EmployeeComponent implements OnInit {
-  
+  token : tokenPara;
   constructor(private employeeService:EmployeeService) { }
   ngOnInit() {
     
   }
 
   addUser(email: string, password: string) {
-    if (this.employeeService.addMysqlUserDatas(email, password)) {
+    if (this.employeeService.addMysqlUserDatas(email, password).subscribe(res => {
+        localStorage.setItem('token', res['token']);
+        console.log(typeof(res));
+        
+      })) {
       console.log(email,password)
-      //alert('Data Inserted Successfully');
+      alert('Log In Successfully');
+      
+    }
+    else{
+      alert('mail id or password is incorrect');
     }
   }
+   
+ 
   
 }
