807d8dd42673f72eafea20beb82fbeaa14615274
vrushali05 <vrushali05@gmail.com>
2019-03-29 21:51:39 +0530

    added user image and name

--- FinalProject/src/app/contact-person/contact-person.component.ts
+++ FinalProject/src/app/contact-person/contact-person.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ContactPersonService } from './contact-person.service';
+import { GlobalVariableService } from '../global-variable.service'
+
 @Component({
   selector: 'app-contact-person',
   templateUrl: './contact-person.component.html',
@@ -8,9 +10,9 @@ import { ContactPersonService } from './contact-person.service';
 })
 export class ContactPersonComponent implements OnInit {
 
-  constructor(private router:Router,private contactPersonService:ContactPersonService ) { }
+    constructor(public gv: GlobalVariableService, private router:Router,private contactPersonService:ContactPersonService ) { }
 
-  ngOnInit() {
+    ngOnInit() {
     this.loadScript('http://www.some-library.com/library.js');
     this.loadScript('./src/assets/popup.js');
   }
@@ -23,14 +25,14 @@ export class ContactPersonComponent implements OnInit {
       script.async = false;
       script.defer = true;
       body.appendChild(script);
-    } 
+    }
 
   contactInfo(){
     this.contactPersonService.contactInfo(localStorage.getItem('token')).subscribe(res => {
       console.log(res);
       console.log(window.location.href  )
-     
-    }); 
+
+    });
   }
 
 }
